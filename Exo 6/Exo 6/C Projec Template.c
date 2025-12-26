6288c4edd233b1c9f840d3aecc69a210325648a3
DrouicheMouad <lycinal100622010843400@gmail.com>
2025-12-26 19:56:59 +0100

    Update Exo 6

--- Exo 6/Exo 6/C Projec Template.c
+++ Exo 6/Exo 6/C Projec Template.c
@@ -1,25 +1,13 @@
 #include <stdio.h>
-#include <math.h>
-#include <stdbool.h>
+void Bin(int n) {
+	if (n == 0) {
+		return;
+	}
+	printf("%d", n % 2);
+	return Bin(n / 2);
+}
+
 int main() {
-	int DecimalValue = 0;
-	printf("Enter an unsigned decimal number "); scanf("%d", &DecimalValue);
-	if (DecimalValue == 0) {
-		printf("The binary value is 0\n");
-	}
-	else {
-		int NumberOfBits = ceil(log(DecimalValue + 1) / log(2));
-		printf("we need at least %d bits to represent this value", NumberOfBits);
-		bool BinaryValue[NumberOfBits];
-		int i = 0;
-		while (DecimalValue > 0) {
-			BinaryValue[i] = DecimalValue % 2;
-			DecimalValue = (DecimalValue - BinaryValue[i]) / 2;
-			i++;
-		}
-		printf("\nThe binary value is ");
-		for (int j = NumberOfBits - 1; j >= 0; j--)
-			printf("%d", BinaryValue[j]);
-	}
+	Bin(7);
 	return 0;
 }
